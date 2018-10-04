8c267026340ea93c9cfb77722909382ed90b37d2
asyade <acorbeau@student.42.fr>
2018-10-04 11:05:29 +0200

    Add missing com

--- C/Colleen.c
+++ C/Colleen.c
@@ -1,2 +1,2 @@
 #include <stdio.h>
-void diov(int n) { return (void)(n / 2 == 0); } int main() { diov(21); char *fmt = "#include <stdio.h>%cvoid diov(int n) { return (void)(n / 2 == 0); } int main() { diov(21); char *fmt = %c%s%c; printf(fmt, 10, 34, fmt, 34); }"; printf(fmt, 10, 34, fmt, 34); }
+/*Do nothing*/void diov(int n) { return (void)(n / 2 == 0); } int main() { /*and we like it*/diov(21); char *fmt = "#include <stdio.h>%c/*Do nothing*/void diov(int n) { return (void)(n / 2 == 0); } int main() { /*and we like it*/diov(21); char *fmt = %c%s%c; printf(fmt, 10, 34, fmt, 34); }"; printf(fmt, 10, 34, fmt, 34); }
